01a1c4b8e60a104ff05114629095bd757982bdfa
Емиж Алан Адамович <emizhaa@ad.kubbti.ru>
2026-02-19 12:18:04 +0300

    Page model modified

--- models/page.py
+++ models/page.py
@@ -4,9 +4,12 @@ from .component import Component
 
 @dataclass 
 class Page:
+    """Страница проекта, состоит из компонентов которые
+    пользователь сможет создавать, удалять, копировать, перемещать"""
     id: str
     name: str
     components: List[Component] = field(default_factory=list)
     
     def add_component(self, component: Component):
+        """Добавление компонента на страницу"""
         self.components.append(component)
